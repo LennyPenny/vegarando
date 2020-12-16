a8531234fe09d60638dcefd05dffff9d29889a17
Lennart Bernhardt <me@lenny.ninja>
2020-12-16 20:22:34 +0100

    use browser polyfill

--- src/popup/vegarandoPopup.js
+++ src/popup/vegarandoPopup.js
@@ -1,8 +1,5 @@
 let hideMeatyCheckbox = document.querySelector("input[id='hideMeaty']");
 
-// oh my god can you please agree on `browser.`
-var browser = window.browser ? window.browser : window.chrome;
-
 hideMeatyCheckbox.addEventListener("click", (e) => {
     browser.storage.sync.set({
         "hideMeaty": hideMeatyCheckbox.checked
